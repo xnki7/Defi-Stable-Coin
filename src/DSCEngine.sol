c7b641f2495186bb0e0a4d0bbe12270ac0b54695
xnki7 <codeinja7@gmail.com>
2025-07-09 10:35:53 +0530

    Mint DSC function

--- src/DSCEngine.sol
+++ src/DSCEngine.sol
@@ -12,6 +12,7 @@ contract DSCEngine is ReentrancyGuard {
     error DSCEngine__TokenNotAllowed();
     error DSCEngine__TransferFailed();
     error DSCEngine__BreaksHealthFactor(uint256 healthFactor);
+    error DSCEngine__MintFailed();
 
     uint256 private constant ADDITIONAL_FEED_PRECESION = 1e10;
     uint256 private constant PRECESION = 1e18;
@@ -79,6 +80,10 @@ contract DSCEngine is ReentrancyGuard {
     function mintDsc(uint256 amountDscToMint) external moreThanZero(amountDscToMint) nonReentrant {
         s_DSCMinted[msg.sender] += amountDscToMint;
         _revertIfHealthFactorIsBroken(msg.sender);
+        bool minted = i_dsc.mint(msg.sender, amountDscToMint);
+        if (!minted) {
+            revert DSCEngine__MintFailed();
+        }
     }
 
     function burnDsc() external {}
